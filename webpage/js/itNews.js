c131141f79bf27ac819c1f8598e69db9d01c7eda
nicoleei <nicoleefang@gmail.com>
2017-11-19 17:15:50 +0800

    js

--- webpage/js/itNews.js
+++ webpage/js/itNews.js
@@ -57,7 +57,8 @@ $(function(){
        }
 
     }
-
+   //图片轮播
+   /*
     var J_imgbox = {
        goLeft:function(){
           var index;
@@ -104,7 +105,96 @@ $(function(){
 
 
     }
+*/
+   var J_imgbox1 = {
+     goLine:function(){
+        let ulWidth = $("#J_imgbox").width();
+        $.map($("#J_imgbox li"),function(item){
+           let flag;
+           if($(item).attr("flag")-0>0){
+              flag = $(item).attr("flag") - 1;
+              
+           }else{
+                 flag = $(item).attr("flag")-0;
+           }
+           let liLeft = ulWidth * flag + "px";
+           $(item).css("left", liLeft);
+           
+            
+        }) 
+     },
+     goLeft:function(){
+        //  let imgbox = $("#J_imgbox");
+        // let imgWth = imgbox.width();
+        // // let oldulLeft = (imgbox.css("left")).replace(/[^0-9]/ig,"") - 0;
+        // let oldulLeft = (imgbox.css("left")).substring(0,(imgbox.css("left")).length-2) - 0;
+        // let newulLeft = oldulLeft + imgWth;
+        // if(oldulLeft>=imgWth){
+        //     newulLeft = -imgWth*2;
+        //     imgbox.css({
+        //        left:newulLeft+"px",
+        //        transition:"left 0s"
+        //     })
+        //     newulLeft = (imgbox.css("left")).substring(0,(imgbox.css("left")).length-2)-0+imgWth;
+        //     imgbox.css({
+        //        left:newulLeft+"px",
+        //        transition:"left 1s"
+        //     })
+          
+          
+        // }
+       
+        // imgbox.css({
+        //    left:newulLeft+"px",
+        //    transition:"left 1s"
+        // })
 
+
+     },
+     goRight:function(){
+        
+        let oldulLeftWidth = $("#J_imgbox").css("left"); 
+        let oldoldulLeftWidthNum = oldulLeftWidth.substring(0,oldulLeftWidth.length-2);
+
+        let ulWidth = $("#J_imgbox").width();
+
+        let newOldLeft = oldoldulLeftWidthNum - ulWidth + "px";
+        if(Math.abs(newOldLeft.replace(/[^0-9]/ig,"")-0)>ulWidth*3){
+
+          // let img1l = oldoldulLeftWidthNum -ulWidth*1 +"px";
+          newOldLeft = 0;
+          
+           $("#J_imgbox").css({
+             "left":newOldLeft,
+             "transition":"left 0s"
+           })
+
+          newOldLeft = 0- ulWidth + "px";
+
+           // $("#J_imgbox").css({
+           //   "left":newOldLeft,
+           //   "transition":"left 1s"
+           // })
+
+
+        }
+         $("#J_imgbox").css({
+             "left":newOldLeft,
+             "transition":"left 1s"
+           })
+      
+        
+        
+        
+     },
+     init:function(){
+      this.goLine();
+
+      $("#J_Left").click(this.goLeft);
+      $("#J_right").click(this.goRight);
+   
+     }
+   }
 
     //搜索框聚焦和失去焦点
     J_title.init()
@@ -112,7 +202,8 @@ $(function(){
     J_test.init()
 
     //图片轮播
-    J_imgbox.init()
+    //J_imgbox.init()
+    J_imgbox1.init()
 })
 
 
